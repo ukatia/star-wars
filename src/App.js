989aa3481dfc07cb4491c2bc81462c3ce6468669
ukatia <esc.katia@gmail.com>
2018-03-31 23:08:41 +0300

    star-wars react app

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import DatatableComponent from './Datatable.js'
+import logo from './logo.svg';
+import './App.css';
+import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/lib/Button';
+
+
+const List = ({ items}) => (
+  <ul>
+    {
+       Object.keys(items).map((e, i) => 
+        (<li><h4>{e}: </h4>{items[e]}</li>))
+    }
+  </ul>
+);
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {showModal: false, description: {}}
+  }
+
+  showDetails(data) {
+    console.log('details')
+    console.log(data)
+    this.setState({ description : data });
+    this.setState({ showModal : true });
+  }
+
+  
+	close(event) {
+    // close modal
+    event.preventDefault();
+    this.setState({ showModal : false });
+  }
+
+  selectCategory(event) {
+     this.refs.datatable.onCategoryChanged(event.target.value);
+  }
+  
+  render() {
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Star Wars</h1>
+          <select onChange={this.selectCategory.bind(this)}>
+            <option value="people">People</option>
+            <option value="planets">Planets</option>
+            <option value="films">Films</option>
+            <option value="species">Species</option>
+            <option value="vehicles">Vehicles</option>
+            <option value="starships">Starships</option>
+        </select>
+        </header>
+        <DatatableComponent className='datatable' name='datatable' ref="datatable" onCellClicked={this.showDetails.bind(this)}></DatatableComponent>
+        <Modal className='modal' show = {this.state.showModal}>
+            <Modal.Header>
+                <Modal.Title> Description </Modal.Title>
+                <Button className='closeBtn' onClick={this.close.bind(this)}>Close</Button>
+            </Modal.Header>
+       
+            <Modal.Body>
+            <List items={this.state.description} />
+	        </Modal.Body>
+	    </Modal>  
+      </div>
+    );
+  }
+}
+
+export default App
+
+
+
---
+++ src/Datatable.js
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+import DataTables from 'material-ui-datatables';
+import {capitalize} from './Utils.js';
+import axios from 'axios';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+ 
+ 
+class DatatableComponent extends Component {
+
+  constructor(props) {
+    // props = TABLE_COLUMNS;
+     super(props)
+
+     this.handlePreviousPageClick = this.handlePreviousPageClick.bind(this);
+     this.handleNextPageClick = this.handleNextPageClick.bind(this);
+         
+    // this.state = { page: 1 }
+     this.state = { nColumns: 5, 
+      columns: [], 
+      data: [],
+      rowsPerPage: [],
+      title: '',
+      page: 1,
+      rowSize: 10,
+      count: 100,
+      category: 'people'
+    }
+   }
+
+  componentDidMount(){
+    this.browseAPI(this.state.page, this.state.category);
+  }
+
+  onCategoryChanged(value) {
+    this.setState({page: 1, category: value, columns: []})
+    this.browseAPI(1, value)
+  }
+
+  handleCellClick = (y, x, row) => {
+    this.props.onCellClicked(this.state.data[y]);
+  }
+
+  // on previous page clicked
+  handlePreviousPageClick() {
+    var currentPage = this.state.page - 1;
+    this.setState({
+      page: currentPage,
+    });
+    this.browseAPI(currentPage, this.state.category);
+  }
+
+  // on next page clicked
+  handleNextPageClick() {
+    var currentPage = this.state.page + 1
+    console.log(currentPage)
+    this.setState({
+      page: currentPage,
+    });
+    this.browseAPI(currentPage, this.state.category);
+  }
+
+  browseAPI (currentPage, category) {
+    
+    axios.get('https://swapi.co/api/' + category + '?page=' + currentPage).then(response => 
+    {
+      response = JSON.parse(JSON.stringify(response))
+      var results = response.data.results;
+      this.setState({data: results});
+      this.setState({count: response.data.count})
+
+      if (results.length > 0) {
+        var i=0;
+        for (var key in results[0]) {
+          if (i < this.state.nColumns && this.state.columns.length < this.state.nColumns) {
+            this.setState({columns: this.state.columns.concat(
+              {
+                key: key,
+                label: capitalize(key)
+              })
+              })
+            }
+          }
+        }
+      })
+  }
+
+  showAlert() {
+    alert('Hello World');
+}
+
+ 
+  render() {
+    return (
+      <MuiThemeProvider  muiTheme={getMuiTheme(darkBaseTheme)}>
+        <DataTables
+          title={this.props.title}
+          height={'auto'}
+          selectable={false}
+          showRowHover={true}
+          columns={this.state.columns}
+          data={this.state.data}
+          showCheckboxes={false}
+          onCellClick={this.handleCellClick}
+          onCellDoubleClick={this.handleCellDoubleClick}
+          onFilterValueChange={this.handleFilterValueChange}
+          onSortOrderChange={this.handleSortOrderChange}
+          onNextPageClick={this.handleNextPageClick}
+          onPreviousPageClick={this.handlePreviousPageClick} 
+          rowSize={this.state.rowSize}
+          page={this.state.page}
+          count={this.state.count}
+      />
+       </MuiThemeProvider>
+    );
+  }
+}
+
+export default DatatableComponent
